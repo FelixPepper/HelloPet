d68aa4ebc6adcd1f81b41fef54adcf7b338f86cb
Tyler <trgo2013@gmail.com>
2018-03-20 21:18:41 -0400

    Initial Commit

---
+++ HelloPet/Data/HelloPetContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloPet.Models
+{
+    public class HelloPetContext : DbContext
+    {
+        public HelloPetContext (DbContextOptions<HelloPetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HelloPet.Models.Pet> Pet { get; set; }
+    }
+}
---
+++ HelloPet/Models/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloPet.Models
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+        public string PetName { get; set; }
+        public string Color { get; set; }
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+        public string Description { get; set; }
+    }
+}
